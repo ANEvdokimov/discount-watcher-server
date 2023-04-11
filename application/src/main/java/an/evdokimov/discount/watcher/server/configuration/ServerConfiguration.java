e322f89cfd335cea8541d433977562fa027b6e9a
Alexey Evdokimov <alexey.nikolaevich.e@gmail.com>
2023-04-11 15:08:59 +0600

    refactoring

--- application/src/main/java/an/evdokimov/discount/watcher/server/configuration/ServerConfiguration.java
+++ application/src/main/java/an/evdokimov/discount/watcher/server/configuration/ServerConfiguration.java
@@ -2,7 +2,6 @@ package an.evdokimov.discount.watcher.server.configuration;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,7 +10,6 @@ import java.time.Clock;
 
 @Configuration
 @EnableScheduling
-@EnableConfigurationProperties
 @ConfigurationPropertiesScan
 public class ServerConfiguration {
     @Bean
--- security-spring-boot-starter/src/main/java/an/evdokimov/securitystarter/security/SecurityStarter.java
+++ security-spring-boot-starter/src/main/java/an/evdokimov/securitystarter/security/SecurityStarter.java
@@ -2,9 +2,11 @@ package an.evdokimov.securitystarter.security;
 
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @AutoConfiguration
 @ComponentScan
+@ConfigurationPropertiesScan
 public class SecurityStarter {
 }
