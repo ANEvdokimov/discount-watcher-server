8427c03903a67fe66b5832321c3d6803ef7d41ef
Alexey Evdokimov <alexey.nikolaevich.e@gmail.com>
2023-05-05 13:54:20 +0600

    refactor sql

--- sql/creating.sql
+++ sql/creating.sql
@@ -44,15 +44,13 @@ CREATE SEQUENCE shop_sequence;
 CREATE TABLE shop
 (
     id            BIGINT PRIMARY KEY,
-    shop_chain_id BIGINT       NOT NULL,
+    shop_chain_id BIGINT       NOT NULL REFERENCES shop_chain (id) ON UPDATE CASCADE ON DELETE RESTRICT,
     name          VARCHAR(255) NOT NULL,
     cyrillic_name VARCHAR(255),
-    city_id       BIGINT       NOT NULL,
+    city_id       BIGINT       NOT NULL REFERENCES city (id) ON UPDATE CASCADE ON DELETE RESTRICT,
     address       VARCHAR(255) NOT NULL,
     cookie        VARCHAR(255),
-    version       BIGINT       NOT NULL,
-    FOREIGN KEY (shop_chain_id) REFERENCES shop_chain (id) ON UPDATE CASCADE ON DELETE RESTRICT,
-    FOREIGN KEY (city_id) REFERENCES city (id) ON UPDATE CASCADE ON DELETE RESTRICT
+    version       BIGINT       NOT NULL
 );
 
 CREATE SEQUENCE product_information_sequence;
@@ -70,12 +68,10 @@ CREATE SEQUENCE product_sequence;
 CREATE TABLE product
 (
     id                     BIGINT PRIMARY KEY,
-    product_information_id BIGINT NOT NULL,
-    shop_id                BIGINT NOT NULL,
+    product_information_id BIGINT NOT NULL REFERENCES product_information (id) ON UPDATE CASCADE ON DELETE RESTRICT,
+    shop_id                BIGINT NOT NULL REFERENCES shop (id) ON UPDATE CASCADE ON DELETE RESTRICT,
     version                BIGINT NOT NULL,
-    UNIQUE (product_information_id, shop_id),
-    FOREIGN KEY (product_information_id) REFERENCES product_information (id) ON UPDATE CASCADE ON DELETE RESTRICT,
-    FOREIGN KEY (shop_id) REFERENCES shop (id) ON UPDATE CASCADE ON DELETE RESTRICT
+    UNIQUE (product_information_id, shop_id)
 );
 CREATE INDEX ON product (product_information_id, shop_id);
 
@@ -83,7 +79,7 @@ CREATE SEQUENCE product_price_sequence;
 CREATE TABLE product_price
 (
     id                       BIGINT PRIMARY KEY,
-    product_id               BIGINT         NOT NULL,
+    product_id               BIGINT         NOT NULL REFERENCES product (id) ON UPDATE CASCADE ON DELETE RESTRICT,
     price                    DECIMAL(10, 2),--TODO money
     discount                 DOUBLE PRECISION,
     price_with_discount      DECIMAL(10, 2),
@@ -91,29 +87,25 @@ CREATE TABLE product_price
     availability_information VARCHAR(255),
     date                     TIMESTAMP,
     parsing_status           PARSING_STATUS NOT NULL,
-    version                  BIGINT         NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES product (id) ON UPDATE CASCADE ON DELETE RESTRICT
+    version                  BIGINT         NOT NULL
 );
 
 CREATE TABLE product_price_lenta
 (
-    id              BIGINT PRIMARY KEY,
-    price_with_card DECIMAL(10, 2),--TODO money
-    FOREIGN KEY (id) REFERENCES product_price (id) ON UPDATE CASCADE ON DELETE CASCADE
+    id              BIGINT PRIMARY KEY REFERENCES product_price (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    price_with_card DECIMAL(10, 2)--TODO money
 );
 
 CREATE SEQUENCE user_product_sequence;
 CREATE TABLE user_product
 (
     id                    BIGINT PRIMARY KEY,
-    user_id               BIGINT  NOT NULL,
-    product_id            BIGINT  NOT NULL,
+    user_id               BIGINT  NOT NULL REFERENCES "user" (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    product_id            BIGINT  NOT NULL REFERENCES product (id) ON UPDATE CASCADE ON DELETE CASCADE,
     monitor_discount      BOOLEAN NOT NULL,
     monitor_availability  BOOLEAN NOT NULL,
     monitor_price_changes BOOLEAN NOT NULL,
     version               BIGINT  NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES "user" (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (product_id) REFERENCES product (id) ON UPDATE CASCADE ON DELETE CASCADE,
     UNIQUE (user_id, product_id)
 );
 CREATE INDEX ON user_product (user_id, product_id);
