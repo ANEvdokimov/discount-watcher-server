bbd3fdc03d7a32ab8c7121686e732c7a0ada20fd
Alexey Evdokimov <alexey.nikolaevich.e@gmail.com>
2023-05-10 13:41:14 +0600

    add main test

---
+++ application/src/test/java/an/evdokimov/discount/watcher/server/MainTest.java
@@ -0,0 +1,12 @@
+package an.evdokimov.discount.watcher.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTest {
+    @Test
+    void contextIsStarted() {
+
+    }
+}
